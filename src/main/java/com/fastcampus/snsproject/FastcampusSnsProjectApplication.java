3a30fddc2d83f71a42d558d3dfc2b299864e83a5
yellow-egg-a <yellow_egg_a@naver.com>
2023-01-26 00:32:24 +0900

    init: initialize project

--- src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
+++ src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
@@ -2,8 +2,9 @@ package com.fastcampus.snsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.
 public class FastcampusSnsProjectApplication {
 
     public static void main(String[] args) {
